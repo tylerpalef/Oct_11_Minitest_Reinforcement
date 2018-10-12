7b76f23e3fe9cf3ccc870d5c966e6e8d39668870
“Tyler <tylerpalef@gmail.com>
2018-10-11 22:41:17 -0400

    added method and test for c to f

--- temperature.rb
+++ temperature.rb
@@ -5,3 +5,7 @@
 def f_to_c(f_temp)
   (f_temp - 32) * 5.0/9.0
 end
+
+def c_to_f(c_temp)
+  c_temp * 9.0/5.0 + 32
+end
--- test_temperature.rb
+++ test_temperature.rb
@@ -7,8 +7,15 @@ class TestTemperature < MiniTest::Test
    def test_f_to_c
      # Not using the test_ naming convention means Minitest won't recognize it is a test
      expected_result = 10
-     actual_result = f_to_c(50)
+     actual_result = f_to_c(50) # act
 
-     assert_equal(expected_result, actual_result) # Put in what you thought (expected) then put in the actual result
+     assert_equal(expected_result, actual_result) # Assert, Put in what you thought (expected) then put in the actual result
+   end
+
+   def test_c_to_f
+     expected_result = 50
+     actual_result = c_to_f(10)
+
+     assert_equal(expected_result, actual_result)
    end
 end
