824c5fc617d320c6c32ea1358f536a08c5a0110d
“Tyler <tylerpalef@gmail.com>
2018-10-11 22:05:29 -0400

    desigining the f to c method

--- test_temperature.rb
+++ test_temperature.rb
@@ -1,7 +1,11 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require_relative 'temperature.rb'
+require 'minitest/autorun' # So we can use methods from minitest to run our tests
+require 'minitest/pride' # So our terminal looks pretty
+require_relative 'temperature.rb' # Need access to code to test it.
 
 class TestTemperature < MiniTest::Test
    # tests go in here
+   def test_f_to_c
+     # Not using the test_ naming convention means Minitest won't recognize it is a test
+     
+   end
 end
